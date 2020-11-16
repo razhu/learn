1e630ed2901f5ddbf0df266c29a8704259bb7e43
ramiro <rhuayller@agetic.gob.bo>
2020-11-16 13:14:54 -0400

    fb login

---
+++ passportfb/index.js
@@ -0,0 +1,52 @@
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.sendFile('auth.html', { root : __dirname}));
+
+const port = process.env.PORT || 3000;
+app.listen(port , () => console.log('App listening on port ' + port));
+
+/*  PASSPORT SETUP  */
+
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/success', (req, res) => res.send("You have successfully logged in"));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+/*  FACEBOOK AUTH  */
+
+const FacebookStrategy = require('passport-facebook').Strategy;
+
+const FACEBOOK_APP_ID = '1073209479798692';
+const FACEBOOK_APP_SECRET = '7c7d04b0e2304ff55a16d4f16946b49b';
+
+passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log('kdsaf profile ', profile);
+      return cb(null, profile);
+  }
+));
+
+app.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/error' }),
+  function(req, res) {
+    res.redirect('/success');
+  });
